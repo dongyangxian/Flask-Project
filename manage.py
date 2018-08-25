139cc799061536e3496c9f8b023e99945a1c7dd1
dongyangxian <huazhilian19@foxmail.com>
2018-08-25 08:37:37 +0800

    0.1-mysql及redis数据库配置

--- manage.py
+++ manage.py
@@ -1,10 +1,32 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 app = Flask(__name__)
+
+class Config(object):
+    DEBUG = True
+
+    # 1. mysql数据库配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:5000/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # 2. redis数据库配置
+    HOST = "127.0.0.1"
+    POST = 6379
+    NUM = 1
+
+app.config.from_object(Config)
+
+# 1.2 创建数据库对象
+db = SQLAlchemy(app)
+
+# 2.2 创建redis实例对象及配置
+redis_store = StrictRedis(host=Config.HOST, port=Config.POST, db=Config.NUM)
 
 @app.route('/')
 def index():
     return 'hello'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
