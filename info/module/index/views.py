cc5ea8ea8af714dcd9648142761ddf0095a2fbb9
dongyangxian <huazhilian19@foxmail.com>
2018-08-28 09:48:58 +0800

    0.12.2-登录接口前后端实现。完善右上角用户登录后显示信息

--- info/module/index/views.py
+++ info/module/index/views.py
@@ -1,24 +1,28 @@
 from flask import session, current_app, render_template
 import logging
+
+from info.models import User
 from . import index_bp
 from info import redis_store, models
 
 # 使用蓝图对象
 @index_bp.route('/')
 def index():
-    session["name"] = "Curry"
 
-    redis_store.set("name", "laowang")
+    # 1. 获取session中保存的用户信息
+    user_id = session.get("user_id")
+    # 2. 根据获取到的id去数据库查询用户的信息
+    user = None  # type:  User
+    if user_id:
+        user = User.query.get(user_id)
 
-    logging.debug("debug 日志信息")
-    logging.info("info 日志信息")
-    logging.warning("warning 日志信息")
-    logging.error("error 日志信息")
-    logging.critical("critical 日志信息")
+    # 3. 将模型信息转化为字典信息
+    data = {
+        "user_info": user.to_dict() if user else None
+    }
+    # 4. 渲染
 
-    current_app.logger.debug("flask封装的logger日志")
-
-    return render_template("index.html")
+    return render_template("index.html", data=data)
 
 @index_bp.route('/favicon.ico')
 def favicon():
