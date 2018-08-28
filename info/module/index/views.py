d85bc92da34156c6df2a5b81069cdacdd7e71617
dongyangxian <huazhilian19@foxmail.com>
2018-08-28 11:25:13 +0800

    0.15-首页左侧新闻列表分类。

--- info/module/index/views.py
+++ info/module/index/views.py
@@ -1,10 +1,68 @@
-from flask import session, current_app, render_template
+from flask import session, current_app, render_template, request, jsonify
 import logging
 
 from info.models import User, News
+from info.utlis.response_code import RET
 from . import index_bp
 from info import redis_store, models
 from info import constants
+
+# 127.0.0.1:5000/news_list
+@index_bp.route('/news_list')
+def get_news_list():
+    """首页新闻分类排行显示"""
+    # 1. 获取参数
+    cid = request.args.get("cid")
+    page = request.args.get("cur_page", 1)
+    per_page = request.args.get("total_page", constants.HOME_PAGE_MAX_NEWS)
+
+    # 2. 参数校验
+    # 2.1 非空判断
+    if not all([cid, page, per_page]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+    # 2.2 数据类型判断
+    try:
+        cid = int(cid)
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="数据类型错误")
+
+    # 3. 逻辑处理
+    # 3.1 根据分类id查询数据，以时间降序，分页处理
+
+    filters = []
+    if cid != 1:
+        filters.append(News.category_id == cid)
+
+    try:
+        paginate = News.query.filter(*filters).\
+            order_by(News.create_time.desc()).paginate(page, per_page, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询首页新闻列表数据异常")
+
+    # 3.2 获取当前页码，当前页码的所有数据，总页数
+    current_page = paginate.page
+    items = paginate.items
+    total_page = paginate.pages
+
+    # 3.3 将模型对象转化为字典对象
+    news_dict_list = []
+
+    for news in items if items else []:
+        news_dict_list.append(news.to_dict())
+
+    # 3.4 组织响应数据
+    data = {
+        "newsList": news_dict_list,
+        "current_page": current_page,
+        "total_page": total_page
+    }
+
+    # 4. 返回值
+    return jsonify(errno=RET.OK, errmsg="查询新闻列表数据成功", data=data)
 
 # 使用蓝图对象
 @index_bp.route('/')
--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,10 +1,13 @@
-var currentCid = 0; // 当前分类 id
+var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
 var data_querying = true;   // 是否正在向后台获取数据
 
 
 $(function () {
+
+    updateNewsData()
+
     // 首页分类切换
     $('.menu li').click(function () {
         var clickCid = $(this).attr('data-cid')
@@ -47,4 +50,30 @@ $(function () {
 
 function updateNewsData() {
     // TODO 更新新闻数据
+    var params = {
+        "cid": currentCid,
+        "cur_page": cur_page
+    }
+    $.get("/news_list", params, function (resp) {
+        if(resp.errno == "0"){
+            // 请求到新闻的列表数据成功回调
+            // 先清空原有数据
+            $(".list_con").html('')
+            // 显示数据
+            for (var i=0;i<resp.data.newsList.length;i++) {
+                var news = resp.data.newsList[i]
+                var content = '<li>'
+                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                content += '<div class="author_info fl">'
+                content += '<div class="source fl">来源：' + news.source + '</div>'
+                content += '<div class="time fl">' + news.create_time + '</div>'
+                content += '</div>'
+                content += '</li>'
+                $(".list_con").append(content)
+            }
+
+        }
+    })
 }
