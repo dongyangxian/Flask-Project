b3184f2cb07fc1c467246f3caa4b4454a97d9d35
dongyangxian <huazhilian19@foxmail.com>
2018-08-29 10:44:48 +0800

    0.19.2-收藏与取消收藏新闻的前后端实现。

--- info/module/news/views.py
+++ info/module/news/views.py
@@ -1,5 +1,5 @@
-from flask import abort
-from flask import current_app, g
+from flask import abort, jsonify
+from flask import current_app, g, request
 from flask import session
 
 from info import constants, db
@@ -7,6 +7,59 @@ from info.models import User, News, Category
 from info.module.news import news_bp
 from flask import render_template
 from info.utlis.common import login_user_data
+
+# 127.0.0.1:5000/news/news_collect
+from info.utlis.response_code import RET
+
+
+@news_bp.route('/news_collect', methods=["POST"])
+@login_user_data
+def news_collect():
+    # 1. 获取参数
+    params_dict = request.json
+    news_id = params_dict.get("news_id")
+    action = params_dict.get("action")
+    user = g.user
+    # 2. 校验参数
+    # 2.1 非空判断
+    if not all([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+    # 2.2 判断用户是否登录
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 2.3 判断前端传送过来的行为是取消还是收藏[collect, cancel_collect]
+    if action not in ["collect", "cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数填写错误")
+
+    # 3. 逻辑处理
+    # 3.1 由新闻的id获取该新闻
+    news = None
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询新闻数据异常")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="新闻不存在")
+    # 3.2 根据action来判断是取消还是收藏
+    if action == "collect":
+        # 3.3 如果是取消就从用户的新闻中移除
+        user.collection_news.append(news)
+    else:
+        # 3.4 如果是收藏就添加进去
+        user.collection_news.remove(news)
+    # 3.5 保存数据到数据库
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="提交数据到数据库异常")
+
+    # 4. 返回值
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 # 127.0.0.1:5000/news/1
 @news_bp.route('/<int:news_id>')
--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -13,14 +13,66 @@ $(function(){
 
     // 收藏
     $(".collection").click(function () {
+        var params = {
+            "news_id": $(this).attr("data-newid"),
+            "action": "collect"
+        }
+        $.ajax({
+            url: "/news/news_collect",
+            type: "post",
+            data: JSON.stringify(params),
+            contentType: "application/json",
+            dataType: "json",
+            headers: {
+                 "X-CSRFToken": getCookie("csrf_token")
+             },
+            success:  function (resp) {
+                if(resp.errno == 0){
+                    // 收藏成功
+                    // 隐藏收藏按钮
+                    $(".collection").hide();
+                    // 显示取消收藏按钮
+                    $(".collected").show();
+                }else if (resp.errno == 4101){
+                    $('.login_form_con').show();
+                }else {
+                    alert(resp.errmsg);
+                }
+            }
 
-       
+        })
     })
 
     // 取消收藏
     $(".collected").click(function () {
+        var params = {
+            "news_id": $(this).attr("data-newid"),
+            "action": "cancel_collect"
+        }
+        $.ajax({
+            url: "/news/news_collect",
+            type: "post",
+            data: JSON.stringify(params),
+            contentType: "application/json",
+            dataType: "json",
+            headers: {
+                 "X-CSRFToken": getCookie("csrf_token")
+             },
+            success:  function (resp) {
+                if(resp.errno == 0){
+                    // 取消收藏成功
+                    // 显示收藏按钮
+                    $(".collection").show();
+                    // 隐藏取消收藏按钮
+                    $(".collected").hide();
+                }else if (resp.errno == 4101){
+                    $('.login_form_con').show();
+                }else {
+                    alert(resp.errmsg);
+                }
+            }
 
-     
+        })
     })
 
         // 评论提交
