338c4e6ee5461144198d561584e080964f4c5f71
dongyangxian <huazhilian19@foxmail.com>
2018-08-30 22:29:13 +0800

    0.22.4-个人中心首页之用户数据修改前后端实现。

--- info/module/profile/views.py
+++ info/module/profile/views.py
@@ -1,9 +1,15 @@
+from flask import current_app
+from flask import session
+
+from info import db
 from info.module.profile import profile_bp
-from flask import render_template, g, request
+from flask import render_template, g, request, jsonify
 
 from info.utlis.common import login_user_data
+from info.utlis.response_code import RET
 
-@profile_bp.route('/base_info')
+
+@profile_bp.route('/base_info', methods=["POST", "GET"])
 @login_user_data
 def base_info():
     """获取用户基本资料，修改用户进本资料页面"""
@@ -13,6 +19,41 @@ def base_info():
             "user_info": user.to_dict() if user else None
         }
         return render_template('news/user_base_info.html', data=data)
+
+    # 如果不是get请求，那就是post请求了
+    # 1. 获取参数
+    params_dict = request.json
+    nick_name = params_dict.get("nick_name")
+    signature = params_dict.get("signature")
+    gender = params_dict.get("gender")
+    # 2. 校验参数
+    #  2.1 非空判断
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+    # 2.2 用户是否登录判断
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+    # 2.3 判断性别
+    if gender not in ["MAN", "WOMAN"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="action数据填写错误")
+    # 3. 逻辑处理
+    # 3.1 用户信息赋值
+    user.nick_name = nick_name
+    user.signature = signature
+    user.gender = gender
+
+    # 3.2 修改session中的昵称
+    session["nick_name"] = nick_name
+
+    # 3.3 保存修改的操作到数据库
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="修改用户数据异常")
+    # 4. 返回值
+    return jsonify(errno=RET.OK, errmsg="修改用户数据成功")
 
 # /user/info
 @profile_bp.route('/info')
