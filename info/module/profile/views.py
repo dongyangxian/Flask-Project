08dba474a0483cc19eaf2d5034a593bab4a418bd
dongyangxian <huazhilian19@foxmail.com>
2018-08-30 21:54:33 +0800

    0.22.1.1-个人中心首页模板抽取。

--- info/module/profile/views.py
+++ info/module/profile/views.py
@@ -1,6 +1,13 @@
 from info.module.profile import profile_bp
-from flask import render_template
+from flask import render_template, g
+
+from info.utlis.common import login_user_data
 
 @profile_bp.route('/base_info')
+@login_user_data
 def user_info():
-    return render_template('news/user.html')
+    user = g.user
+    data = {
+        "user_info": user.todict() if user else  None
+    }
+    return render_template('news/user.html', data=data)
