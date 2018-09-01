c0b5eacbfbd5646e2ef47bd0c96b20bc173ac377
dongyangxian <huazhilian19@foxmail.com>
2018-09-01 17:27:53 +0800

    0.22.12.6-管理员后台-用户统计页面前后端实现。

--- info/module/admin/views.py
+++ info/module/admin/views.py
@@ -1,3 +1,6 @@
+import time
+from datetime import datetime, timedelta
+
 from flask import current_app
 from flask import g
 from flask import session, redirect, url_for
@@ -6,6 +9,82 @@ from info.models import User
 from info.module.admin import admin_bp
 from flask import render_template, request
 from info.utlis.common import login_user_data
+
+@admin_bp.route('/user_count')
+@login_user_data
+def user_count():
+    """展示用户统计页面"""
+    # 查询总人数
+    total_count = 0
+    try:
+        # 10001
+        total_count = User.query.filter(User.is_admin == False).count()
+    except Exception as e:
+        current_app.logger.error(e)
+    # 查询月新增数
+    mon_count = 0
+
+    try:
+        # 2018-8-31号
+        now = time.localtime()
+        # 2018-08-01月初  2018-09-01
+        mon_begin = '%d-%02d-01' % (now.tm_year, now.tm_mon)
+        # 使用strptime将字符串转成时间date格式 '%Y-%m-%d'
+        mon_begin_date = datetime.strptime(mon_begin, '%Y-%m-%d')
+        # 用户的创建时间 > 每一个月的第一天 -->每一个月的新增人数
+        mon_count = User.query.filter(User.is_admin == False, User.create_time >= mon_begin_date).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 查询日新增数
+    day_count = 0
+
+    try:
+        # 2018-08-31-0:0
+        day_begin = '%d-%02d-%02d' % (now.tm_year, now.tm_mon, now.tm_mday)
+        day_begin_date = datetime.strptime(day_begin, '%Y-%m-%d')
+        # 当前时间 > 2018-08-31-0:0(一天的开始时间)
+        day_count = User.query.filter(User.is_admin == False, User.create_time > day_begin_date).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 查询图表信息
+    # 获取到当天00:00:00时间
+    # 2018-08-31
+    now_date = datetime.strptime(datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d')
+    # 定义空数组，保存数据
+    active_date = []
+    active_count = []
+
+    # 依次添加数据，再反转
+    for i in range(0, 31):
+        # 2018-08-31 - 1  =  2018-08-30:0:0
+        # 2018-08-31 - 2  =  2018-08-29:0:0
+        begin_date = now_date - timedelta(days=i)
+        # now_date - timedelta(days=(i))  + timedelta(days=(1))
+        # begin_date + timedelta(days=(1))
+        # 2018-08-30:24:00
+        # 2018-08-29:24:00
+        end_date = begin_date + timedelta(days=1)
+        # 记录这个月的每一天  30 29 ....01
+        # strftime将date格式转换成字符串
+        active_date.append(begin_date.strftime('%Y-%m-%d'))
+        count = 0
+
+        try:
+            count = User.query.filter(User.is_admin == False, User.last_login >= begin_date,
+                                      User.last_login < end_date).count()
+        except Exception as e:
+            current_app.logger.error(e)
+        # 记录一个月的每天的用户活跃数
+        active_count.append(count)
+    # 日期反转
+    active_date.reverse()
+    # 数据反转
+    active_count.reverse()
+    data = {"total_count": total_count, "mon_count": mon_count, "day_count": day_count, "active_date": active_date,
+            "active_count": active_count}
+    return render_template('admin/user_count.html', data=data)
 
 @admin_bp.route('/index')
 @login_user_data
