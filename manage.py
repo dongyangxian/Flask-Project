cae6a4ac3ff4bf0d162ca5adee7295e29b051a8c
dongyangxian <huazhilian19@foxmail.com>
2018-09-01 16:30:14 +0800

    0.22.12.1-创建管理员用户工具封装。

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
-
+from flask import current_app
 from flask_script import Manager
 from info import create_app, db
 from flask_migrate import Migrate, MigrateCommand
+
+from info.models import User
 
 app = create_app("development")
 
@@ -12,6 +14,28 @@ manager = Manager(app)
 Migrate(app, db)
 # 添加指令到manager对象
 manager.add_command("db", MigrateCommand)
+
+# useage: python manage.py createsuperuser -n "admin" -p "123456"
+@manager.option("-n", "-name", dest="name")
+@manager.option("-p", "-password", dest="password")
+def createsuperuser(name, password):
+    """创建管理员用户"""
+    if not all([name, password]):
+        return "参数不足"
+    user = User()
+    user.is_admin = True
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        print("创建管理员失败")
+
+    print("创建管理员用户成功")
 
 if __name__ == '__main__':
     manager.run()
